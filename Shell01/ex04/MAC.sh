e470d787bbc629dcbff1b4900330b7a5206fd749
Alberto Florido <143635386+aflr@users.noreply.github.com>
2023-09-17 21:43:43 +0200

    Add files via upload

---
+++ Shell01/ex04/MAC.sh
@@ -0,0 +1,1 @@
+ifconfig | grep ether | grep -Eo "([[:xdigit:]]{2}:){5}[[:xdigit:]]{2}";
---
+++ Shell01/ex07/r_dwssap.sh
@@ -0,0 +1,1 @@
+cat /etc/passwd | grep -Ev "^#" | awk '!(NR%2)' | grep -Eo "^[^:]*" | grep -Ev "^$" | rev | sort -r | awk -v l1=$FT_LINE1 -v l2=$FT_LINE2 'NR >= l1 && NR <= l2' | awk '{printf $0 ", "}' | sed 's/, $/./' | tr -d '\n';
